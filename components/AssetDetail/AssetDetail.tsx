a5e76aed2a5fbbaa1b5bfc01428cab56ae2f478c
Andrew Cassetti <amcassetti@gmail.com>
2021-02-15 16:24:08 -0500

    manual rebase

--- components/AssetDetail/AssetDetail.tsx
+++ components/AssetDetail/AssetDetail.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AssetDetailStatus } from './constants';
-import DefaultVaultCard from './states/DefaultAssetDetail';
+import DefaultAssetDetail from './states/DefaultAssetDetail';
+import PendingAssetDetail from './states/PendingAssetDetail';
 import type { AssetDetailProps } from './types';
 
 const AssetDetail = ({ status, ...props }: AssetDetailProps) => {
-  return <DefaultVaultCard {...props} />;
+  if (status === AssetDetailStatus.PENDING) {
+    return <PendingAssetDetail />;
+  }
+
+  return <DefaultAssetDetail {...props} />;
 };
 
 export default AssetDetail;
