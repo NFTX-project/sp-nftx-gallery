92646cacba9a18e868a7ce3876d7f709a03a2a6b
Justin Avery <justin@simplethin.gs>
2021-03-24 13:44:34 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'none'; script-src 'self' www.googletagmanager.com www.google-analytics.com; connect-src 'self' nftx.ethereumdb.com; img-src 'self' cms.nftx.xyz res.cloudinary.com; style-src 'self';base-uri 'self';form-action 'self'",
+          },
+        ],
+      },
+    ];
+  },
   // Enable this when we're actually using the nextjs server and not static
   i18n: {
     locales: ['en'],
