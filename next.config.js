7704d261a44978e1dacaaf3f45d97276ddc0b2a1
Justin Avery <justin@simplethin.gs>
2021-03-25 13:19:43 +0000

    added next-secure-headers
    next-secure-headers adds the ability to set security headers to next.js apps.

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
+// /next.config.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createSecureHeaders } = require('next-secure-headers');
+
 module.exports = {
   async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'none'; script-src 'self' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com; font-src 'self'; connect-src 'self' nftx.ethereumdb.com api.covalenthq.com api.opensea.io www.google-analytics.com stats.g.doubleclick.net; img-src 'self' cms.nftx.xyz res.cloudinary.com www.google-analytics.com lh3.googleusercontent.com www.google.com www.google.co.uk; style-src 'self' 'unsafe-inline'; base-uri 'self';form-action 'self'",
-          },
-        ],
-      },
-    ];
+    return [{ source: '/(.*)', headers: createSecureHeaders() }];
   },
+
   // Enable this when we're actually using the nextjs server and not static
   i18n: {
     locales: ['en'],
