97c3fc735d58ab884c8e082a5c7cb117fef057b3
Tobias <4254570+quagliero@users.noreply.github.com>
2021-04-04 20:00:51 +0100

    feat: allow vault routing by vaultId

--- app/hooks/useFund.ts
+++ app/hooks/useFund.ts
@@ -4,19 +4,20 @@ import { getFundKey } from '@/utils/getFundKey';
 
 /**
  * Given a fundKey it returns the whole fund
- * @param fundKey the key (getFundKey) of the fund
+ * @param fundKey the key (getFundKey) or the vault ID of the fund
  *
- * If not fund is found it returns false, this allows for pending states to map
+ * If no fund is found it returns false, this allows for pending states to map
  * to null and failed states to use 'false'
  */
-const useFund = (fundKey: string) => {
+const useFund = (fundKey: string | number) => {
   const [fund, setFund] = useState(null);
   const funds = useFundsContext();
 
   useEffect(() => {
     if (funds.length) {
       const match = funds.find((v) => {
-        return fundKey === getFundKey(v);
+        // if no match on fund key string, try vaultId
+        return fundKey === getFundKey(v) || Number(fundKey) === v.vaultId;
       });
 
       if (match) {
