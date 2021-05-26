97d19b6bdb0ab6584f9a9df426f5bbee7ead48c6
Justin <justincavery@gmail.com>
2021-05-26 12:05:48 +0100

    Dark class body
    Added Dark class to the body by default.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,7 @@ class MyDocument extends Document {
             </>
           )}
         </Head>
-        <body>
+        <body className="dark">
           <Main />
           <NextScript />
           <script
