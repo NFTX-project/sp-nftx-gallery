53ef1b853bfac503815cb98854a03eef0c371347
Justin <justincavery@gmail.com>
2021-06-03 15:59:33 +0100

    Update LightMode.story.tsx

--- app/components/elements/LightMode/LightMode.story.tsx
+++ app/components/elements/LightMode/LightMode.story.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Logo, { LogoProps } from './LightMode';
+import LightMode, { LightModeProps } from './LightMode';
 
 export default {
   title: 'Components/elements/LightMode',
-  component: Logo,
+  component: LightMode,
   argTypes: {
     size: { control: 'number' },
   },
