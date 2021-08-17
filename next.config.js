d3f7081479b81584751f184048df11056c89b421
Justin <justincavery@gmail.com>
2021-08-17 18:01:50 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // Enable this when we're actually using the nextjs server and not static
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
@@ -8,20 +7,5 @@ module.exports = {
   env: {
     appVersion: process.env.npm_package_version,
   },
-  // for @netlify/plugin-next-js
   target: 'serverless',
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: 'https://app.nftx.org',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/funds/waifusion/',
-  //       destination: 'https://app.nftx.org/waifus',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
 };
