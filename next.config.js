96b6ce5be7c68898b224a0cf8909725fb8e90829
Justin <justincavery@gmail.com>
2021-08-18 11:34:21 +0100

    Feat/vercel redirects (#102)
    * Create vercel.json
    
    * Update vercel.json
    
    * Add redirects to next.config.js
    
    * Update next.config.js
    
    * redirect /javery
    
    * Update vercel.json
    
    * Update vercel.json
    
    * Update vercel.json
    
    * Update vercel.json
    
    * updates
    
    * as
    
    * Update next.config.js
    
    * remove semicolon
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,293 @@
 module.exports = {
-  // Enable this when we're actually using the nextjs server and not static
+  async redirects() {
+    return [
+      {
+        source: '/funds/waifusion/',
+        destination:
+          'https://app.nftx.org/redeem/0xe7f4c89032a2488d327323548ab0459676269331/',
+        permanent: true,
+      },
+      {
+        source: '/funds/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-basic/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-attr-4/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-attr-5/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-zombie/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-female/',
+        destination:
+          'https://app.nftx.org/redeem/0x269616d549d7e8eaa82dfb17028d0b212d11232a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/bored-ape-yacht-club-nftx/',
+        destination:
+          'https://app.nftx.org/redeem/0xea47b64e1bfccb773a0420247c0aa0a3c1d2e5c5/',
+        permanent: true,
+      },
+      {
+        source: '/funds/mask/',
+        destination:
+          'https://app.nftx.org/redeem/0xc7a8b45e184138114e6085c82936a8db93dd156a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/zenft/',
+        destination:
+          'https://app.nftx.org/redeem/0xf0fe1a87abf12099b77352bdcc10f5a87067b290/',
+        permanent: true,
+      },
+      {
+        source: '/funds/kamamixer/',
+        destination:
+          'https://app.nftx.org/redeem/0xb2c62e0a1f5837356e399359ecc34fcc49a02093/',
+        permanent: true,
+      },
+      {
+        source: '/funds/gutter-cat-gang/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/the-alien-boy/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/my-fucking-pickle/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/empyrean/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/stadia/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/meebits/',
+        destination:
+          'https://app.nftx.org/redeem/0x641927e970222b10b2e8cdbc96b1b4f427316f16/',
+        permanent: true,
+      },
+      {
+        source: '/funds/apparitions/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/pownft/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/deafbeef-synth-poems/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/solos/',
+        destination:
+          'https://app.nftx.org/redeem/0x32fb2a84af5515f77515806ea5addb54c923237d/',
+        permanent: true,
+      },
+      {
+        source: '/funds/0xmons/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/89-seconds-atomized/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/alpaca-city/',
+        destination:
+          'https://app.nftx.org/redeem/0x413f6a89ddb823974f24f2e3cd3b4f467d3bbb38/',
+        permanent: true,
+      },
+      {
+        source: '/funds/avastar-basic/',
+        destination:
+          'https://app.nftx.org/redeem/0xdcdc1c1cc33aa817cbdbe8f5e2390bf7cc43dc4b/',
+        permanent: true,
+      },
+      {
+        source: '/funds/avastar-rank-30/',
+        destination:
+          'https://app.nftx.org/redeem/0xdcdc1c1cc33aa817cbdbe8f5e2390bf7cc43dc4b/',
+        permanent: true,
+      },
+      {
+        source: '/funds/avastar-rank-60/',
+        destination:
+          'https://app.nftx.org/redeem/0xdcdc1c1cc33aa817cbdbe8f5e2390bf7cc43dc4b/',
+        permanent: true,
+      },
+      {
+        source: '/funds/babes-basic/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/bganpunk/',
+        destination:
+          'https://app.nftx.org/redeem/0xc3b5284b2c0cfa1871a6ac63b6d6ee43c08bdc79/',
+        permanent: true,
+      },
+      {
+        source: '/funds/blockcities/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/chromie-squiggle/',
+        destination:
+          'https://app.nftx.org/redeem/0x8d137e3337eb1b58a222fef2b2cc7c423903d9cf/',
+        permanent: true,
+      },
+      {
+        source: '/funds/color-study/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/cryptoarte/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/cryptocrtstal/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/cubes/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/dada-946/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/ether-cards/',
+        destination:
+          'https://app.nftx.org/redeem/0xd6f7db7db116170ecb1c3b35f12c1e693ba70586/',
+        permanent: true,
+      },
+      {
+        source: '/funds/etheroes/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/glyph/',
+        destination:
+          'https://app.nftx.org/redeem/0xd70240dd62f4ea9a6a2416e0073d72139489d2aa/',
+        permanent: true,
+      },
+      {
+        source: '/funds/joy/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/kitty-gen-0-fast/',
+        destination:
+          'https://app.nftx.org/redeem/0x9b9fb226e98c4e90db2830c9aefa9cfcbe3b000a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/kitty-gen-0/',
+        destination:
+          'https://app.nftx.org/redeem/0x9b9fb226e98c4e90db2830c9aefa9cfcbe3b000a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/mooncats-rescue/',
+        destination:
+          'https://app.nftx.org/redeem/0x98968f0747e0a261532cacc0be296375f5c08398/',
+        permanent: true,
+      },
+      {
+        source: '/funds/niftydudes/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/pepemon/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/pixl-basic/',
+        destination:
+          'https://app.nftx.org/redeem/0xf8eb56c7e4e3c885c905a19583bf41644d35aa0a/',
+        permanent: true,
+      },
+      {
+        source: '/funds/punk-body-index/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/r3sonance/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/singularity/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/twerky-pepe/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+      {
+        source: '/funds/variant-plan-f01y20/',
+        destination: 'https://app.nftx.org',
+        permanent: true,
+      },
+    ];
+  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
@@ -8,6 +296,5 @@ module.exports = {
   env: {
     appVersion: process.env.npm_package_version,
   },
-  // for @netlify/plugin-next-js
   target: 'serverless',
 };
